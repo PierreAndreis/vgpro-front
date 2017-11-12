9895cabbd877b3acb855da19266aae5ad0d3539b
4ever <pierreandreisortega@gmail.com>
2017-11-11 17:47:39 -0800

    Player: Heroes Played

--- src/components/Profile/Sidebar/HeroesPlayed.jsx
+++ src/components/Profile/Sidebar/HeroesPlayed.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-const HeroesPlayed = ({t}) => (
+import "./HeroesPlayed.css";
+
+const Heroes2 = ({t}) => (
   <div className="champions">
     <div className="hero_each" data-hero="' + key.toLowerCase() + '">
     <div className="hero_img" style={{backgroundImage: "url(http://vgpro.gg/assets/images/heroes/adagio.gif)"}}></div>
@@ -12,5 +14,34 @@ const HeroesPlayed = ({t}) => (
       <div className="hero_games">124 {t("sidebar-played")}</div></div>
   </div>
 );
+
+const Hero = () => (
+  <div className="PlayerHero">
+    <div className="PlayerHero-Image" style={{backgroundImage: "url(http://vgpro.gg/assets/images/heroes/adagio.gif)"}} />
+    <div className="PlayerHero-Info">
+      <div className="PlayerHero-Name">Vox</div>
+      <div className="PlayerHero-cs">25 cs</div>
+    </div>
+    <div className="PlayerHero-Stats">
+      <div className="PlayerHero-KDA">2.61 KDA</div>
+      <div className="PlayerHero-KDA-details">
+      <span>1</span>/<span id="deaths">2</span>/<span>32</span>
+      </div>
+    </div>
+    <div className="PlayerHero-WR">
+      <div className="PlayerHero-WR-value">32%</div>
+      <div className="PlayerHero-WR-desc">32 played</div>
+    </div>
+  </div>
+)
+
+const HeroesPlayed = () => (
+  <div className="PlayerHeroes">
+    <Hero/>
+    <Hero/>
+    <Hero/>
+    <Hero/>
+  </div>
+)
 
 export default HeroesPlayed;

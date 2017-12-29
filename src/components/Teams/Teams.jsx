bbab627e2c20a49ce44e8ff7523c770c65be2be7
4ever <pierreandreisortega@gmail.com>
2017-12-29 01:21:35 -0500

    Favicons

--- src/components/Teams/Teams.jsx
+++ src/components/Teams/Teams.jsx
@@ -1,33 +1,3 @@
 import React from "react";
 
-import "./teams.base.css";
-import "./teams.resp.css";
-
-class Team extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="teams_up">
-          <div className="wrap">
-            <div className="regieach">
-              <div className="select" id="na">
-                NA
-              </div>
-              <div id="eu">EU</div>
-              <div id="ea">EA</div>
-              <div id="sg">SEA</div>
-              <div id="sa">SA</div>
-            </div>
-          </div>
-        </div>
-        <div className="content">
-          <div className="wrap">
-            <div className="boxes" />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Team;
+export default () => (<p>Soon</p>)

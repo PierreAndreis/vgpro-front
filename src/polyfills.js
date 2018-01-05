af5e0ff2a8d513c5efd0ad9abef507b778a5ecd7
4ever <pierreandreisortega@gmail.com>
2018-01-04 22:59:15 -0500

    More polyfills

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,9 +1,20 @@
 // import "babel-polyfill";
 
+// import "core-js/es6"
+// import "core-js/es5"
+
 import "core-js/fn/set";
+
+import "core-js/es6/array"
 import "core-js/fn/array/fill";
 import "core-js/fn/array/find";
 import "core-js/fn/array/filter";
-import "core-js/fn/array/forEach";
+import "core-js/fn/array/for-each";
+import "core-js/fn/array/of";
+import "core-js/fn/array/reduce";
+import "core-js/fn/array/slice";
+
 import "core-js/fn/array/includes";
 import "core-js/fn/number/is-nan";
+import "core-js/fn/number/parseInt";
+import "core-js/fn/number/parseFloat";

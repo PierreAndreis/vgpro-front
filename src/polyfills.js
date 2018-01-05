9a8b9b93cf94029750c36219a69e6f00ccffde97
4ever <pierreandreisortega@gmail.com>
2018-01-04 22:31:40 -0500

    Replacing Polyfills

---
+++ src/polyfills.js
@@ -0,0 +1,8 @@
+// import "babel-polyfill";
+
+import "core-js/fn/array/fill";
+import "core-js/fn/array/find";
+import "core-js/fn/array/filter";
+import "core-js/fn/set";
+import "core-js/fn/array/includes";
+import "core-js/fn/number/is-nan";

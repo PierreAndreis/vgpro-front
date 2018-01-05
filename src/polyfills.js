d0010dc8da1aba00ed06ab3f1ee676f3719af10d
4ever <pierreandreisortega@gmail.com>
2018-01-04 22:48:24 -0500

    adding forEach support

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,8 +1,9 @@
 // import "babel-polyfill";
 
+import "core-js/fn/set";
 import "core-js/fn/array/fill";
 import "core-js/fn/array/find";
 import "core-js/fn/array/filter";
-import "core-js/fn/set";
+import "core-js/fn/array/forEach";
 import "core-js/fn/array/includes";
 import "core-js/fn/number/is-nan";

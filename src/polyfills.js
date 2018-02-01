c29f68b8a3c55a1ec18a9ecee0696592aea5c46e
Pierre Ortega <pierreandreisortega@gmail.com>
2018-01-31 20:58:51 -0800

    Polyfill to String.includes

--- src/polyfills.js
+++ src/polyfills.js
@@ -13,8 +13,9 @@ import "core-js/fn/array/for-each";
 import "core-js/fn/array/of";
 import "core-js/fn/array/reduce";
 import "core-js/fn/array/slice";
-
 import "core-js/fn/array/includes";
+import "core-js/fn/string/includes";
+
 import "core-js/fn/number/is-nan";
 import "core-js/fn/number/parse-int";
 import "core-js/fn/number/parse-float";

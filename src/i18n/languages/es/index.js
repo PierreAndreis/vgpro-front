ad79ee6ba0e29a5651d31133aa243209cbe26dd2
Skillz <gankstarsleague@gmail.com>
2018-05-31 00:01:56 -0400

    Translating new heroes strings still missing 2

--- src/i18n/languages/es/index.js
+++ src/i18n/languages/es/index.js
@@ -2,12 +2,15 @@ export default {
     "general.errorMessageDefault": "¡Lo sentimos, un error a ocurrido!",
     "general.lastUpdated": "Última actualización:",
     "general.tryAgain": "Por favor, intente más tarde.",
+    "general.noMatches": "Ningún partido encontrado",
     "general.Next": "Siguiente",
     "general.Back": "Anterior",
     "general.More": "Más",
     "general.ViewMore": "Ver más",
     "general.ViewLess": "Ver menos",
     "general.Reset": "Reiniciar",
+
+    "general.sortBy": "Ordenado por",
 
     "footer.copy":
         "VGPROgg no esta afiliado o es parte de Super Evil Megacorp.  Todos los derechos de autor y arte original de Vainglory son propiedad de Super Evil Megacorp.",
@@ -20,6 +23,10 @@ export default {
     "terms.GameMode": "Modo de Juego",
     "terms.Region": "Región",
     "terms.Search": "Buscar",
+
+    "terms.average": "Average",
+    "terms.placement": "Posición",
+    "terms.name": "Nombre",
 
     "terms.AVG": "AVG",
     "terms.KDA": "AMA",
@@ -71,6 +78,17 @@ export default {
     "terms.weapon": "Arma",
     "terms.utility": "Utilidad",
 
+    "terms.carry": "Carril",
+    "terms.captain": "Capitan",
+    "terms.jungler": "Jungla",
+
+    "terms.tier": "Tier {{tier}}",
+    "terms.op": "OP",
+    "terms.bad": "Malo",
+    "terms.type": "Tipo",
+    "terms.Average": "Average",
+    "terms.Placement": "Posición",
+
     "terms.All": "Todo", // All Region
 
     "terms.Top": "Max {{term}}", // Top Win Rate, Top Pick Rate, Top Ban Rate, Top 4
@@ -80,6 +98,8 @@ export default {
     "gamemode.blitz": "Blitz",
     "gamemode.ranked": "Clasificatoria 3v3",
     "gamemode.ranked5v5": "Clasificatoria 5v5",
+    "gamemode.casual": "Casual 3v3",
+    "gamemode.casual5v5": "Casual 5v5",
     "gamemode.private": "Privada 3v3",
     "gamemode.private5v5": "Privada 5v5",
     "gamemode.privateranked": "Privada Draft",
@@ -88,7 +108,9 @@ export default {
     "gamemode.privatebattleRoyale": "Battle Royale Privada",
 
     "tab.Overview": "General",
+    "tab.Abilities": "Habilidades",
     "tab.Builds": "Construcción",
+    "tab.Heroes": "Héroes",
 
     "terms.OverallStats": "Total Estadísticas",
     "terms.OverviewStats": "General Estadísticas",
@@ -101,5 +123,26 @@ export default {
     "profile.PlayerOf": "Jugador de",
     "profile.Roles": "Roles",
 
-    "search.placeholder": "Buscar Nombre del Jugador",
+    "heroes.mostPlayedAs": "Most Played as",
+    "heroes.highestWinSkill": "Mayor % de Victoria por Orden de Habilidad",
+    "heroes.mostFrequentSkill": "Más Frecuente por Orden de Habilidad",
+    "heroes.mostFrequentBuild": "Construcción más frecuente",
+    "heroes.highestWinBuild": "Mayor % de Victoria por Construcción",
+    "heroes.winRateByPatch": "Tasa de Victoria por Parche",
+    "heroes.banRateByPatch": "Tasa de Baneo por Parche",
+    "heroes.pickRateByPatch": "Tasa de Elección por Parche",
+
+    "heroes.winRateByTime": "Tasa de Victorias en el tiempo",
+    "heroes.banRateByTime": "Tasa de Baneo en el tiempo",
+    "heroes.pickRateByTime": "Tasa de Elección en el tiempo",
+
+    "heroes.winRateByGameLength": "Tasa de Baneo por la Longitud del partido",
+
+    "heroes.playingAgainst": "Jugados contra",
+    "heroes.playingWith": "Jugados con",
+
+    "search.placeholder": "Buscar Nombre de Jugador",
+
+    "filters.role": "Filtrar por Rol",
+
 };

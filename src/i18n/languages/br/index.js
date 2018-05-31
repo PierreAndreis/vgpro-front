85a74ffde76f03045eb501bcb1fc24a19894ee44
Skillz <gankstarsleague@gmail.com>
2018-05-31 00:36:43 -0400

    base portugeses translations missing heroes strings

---
+++ src/i18n/languages/br/index.js
@@ -0,0 +1,151 @@
+export default {
+    "general.errorMessageDefault": "Desculpe, ocorreu um erro!",
+    "general.lastUpdated": "Ultima atualização:",
+    "general.tryAgain": "Por favor, tente novamente mais tarde.",
+    "general.noMatches": "",
+    "general.Next": "Próximo",
+    "general.Back": "Voltar",
+    "general.More": "Mais",
+    "general.ViewMore": "Vizualizar Mais",
+    "general.ViewLess": "Vizualizar Menos",
+    "general.Reset": "Restabelecer",
+
+    "general.sortBy": "",
+
+    "footer.copy":
+        "VGPROgg não é afiliado ou parte de Super Evil Megacorp. Todos os direitos autorais e objetos de arte de Vainglory pertencem à Super Evil Megacorp.",
+
+    "feature.ProHistory": "História Pro",
+    "feature.Leaderboard": "Entre os melhores",
+    "feature.Home": "Início",
+    "feature.Heroes": "Heróis",
+
+    "terms.GameMode": "Modo de Jogo",
+    "terms.Region": "Região",
+    "terms.Search": "Busca",
+
+    "terms.average": "",
+    "terms.placement": "",
+    "terms.name": "",
+
+    "terms.AVG": "AVG",
+    "terms.KDA": "KDA",
+    "terms.MVP": "MVP",
+    "terms.Win": "Vitória",
+    "terms.Loss": "Derrota",
+    "terms.Wins": "Vitórias",
+    "terms.Losses": "Derrotas",
+    "terms.share": "Compartilhar",
+    "terms.cs": "cs",
+    "terms.gold": "ouro",
+    "terms.csMin": "cs/min",
+    "terms.goldMin": "ouro/min",
+    "terms.DamageTaken": "Dano recebido",
+    "terms.DamageDone": "Dano causado",
+    "terms.HealingDone": "Cura Concluída",
+    "terms.Kills": "Matou",
+    "terms.Deaths": "Morreu",
+    "terms.Assists": "Assistências",
+    "terms.KillParticipantion": "Participação na Morte",
+    "terms.Minions": "Minions",
+
+    "terms.perMin": "por min",
+
+    "terms.RedTeam": "Time Vermelho",
+    "terms.BlueTeam": "Time Azul",
+
+    "terms.Items": "Items",
+    "terms.Stats": "Estatísticas",
+    "terms.Rank": "Classificação",
+    "terms.Skills": "Habilidades",
+    "terms.Overdrive": "Overdrive",
+    "terms.Builds": "Builds",
+    "terms.aka": "Também conhecido como:",
+    "terms.global": "Global",
+    "terms.played": "Jogado",
+
+    "terms.games": "{{count}} jogo",
+    "terms.games_plural": "{{count}} jogos",
+
+    "terms.Favorites": "Favoritos",
+    "terms.RecentSearches": "Buscas Recentes",
+
+    "terms.pickrate": "Taxa de Escolha",
+    "terms.banrate": "Taxa de Banimento",
+    "terms.winrate": "Taxa de Vitória",
+
+    "terms.crystal": "Cristal",
+    "terms.weapon": "Arma",
+    "terms.utility": "Utilitário",
+
+    "terms.carry": "Carry",
+    "terms.captain": "Captain",
+    "terms.jungler": "Jungler",
+
+    "terms.tier": "Tier {{tier}}",
+    "terms.op": "OP",
+    "terms.bad": "Bad",
+
+    "terms.type": "Type",
+    "terms.Average": "Average",
+    "terms.Placement": "Placement",
+
+    "terms.All": "Todos", // All Region
+
+    "terms.Top": "Topo {{term}}", // Top Win Rate, Top Pick Rate, Top Ban Rate, Top 4
+    "terms.Points": "Pontos",
+
+    "gamemode.battleroyale": "Batalha real",
+    "gamemode.blitz": "Blitz",
+    "gamemode.ranked": "Classificação 3v3",
+    "gamemode.ranked5v5": "Classificação 5v5",
+    "gamemode.casual": "",
+    "gamemode.casual5v5": "",
+    "gamemode.private": "Privada 3v3",
+    "gamemode.private5v5": "Privada 5v5",
+    "gamemode.privateranked": "Privada Draft",
+    "gamemode.privateranked5v5": "Privada Draft 5v5",
+    "gamemode.privateblitz": "Privada Blitz",
+    "gamemode.privatebattleRoyale": "Privada Batalha Real",
+
+    "tab.Overview": "Visão Geral",
+    "tab.Abilities": "Abilities",
+    "tab.Builds": "Builds",
+    "tab.Heroes": "Heroes",
+
+    "terms.OverallStats": "Estatísticas gerais",
+    "terms.OverviewStats": "Visão geral Estatísticas",
+
+    "profile.notFound": "{{name}} não foi encontrado.",
+    "profile.foundButNoMatches":
+        "{{name}} foi encontrado, mas não jogou um jogo em um tempo",
+    "profile.PlayedWith": "Jogado com",
+    "profile.Heroes": "Heróis",
+    "profile.PlayerOf": "Jogador De",
+    "profile.Roles": "Papéis",
+
+    "heroes.mostPlayedAs": "",
+    "heroes.highestWinSkill": "",
+    "heroes.mostFrequentSkill": "",
+    "heroes.mostFrequentBuild": "",
+    "heroes.highestWinBuild": "",
+    "heroes.winRateByPatch": "",
+    "heroes.banRateByPatch": "",
+    "heroes.pickRateByPatch": "",
+
+    "heroes.winRateByTime": "",
+    "heroes.banRateByTime": "",
+    "heroes.pickRateByTime": "",
+
+    "heroes.winRateByGameLength": "",
+
+    "heroes.playingAgainst": "",
+    "heroes.playingWith": "",
+
+    "search.placeholder": "Pesquisar nome do jogador",
+
+    "filters.role": "",
+
+    situationalItemTitle: "",
+    situationalItem: ``,
+};

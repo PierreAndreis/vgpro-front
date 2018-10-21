6c511c0f5a0804270424241f9e5b669ceee23632
Pierre Ortega <pierreandreisortega@gmail.com>
2018-10-20 19:18:08 -0700

    Fix missing class

--- src/components/common/ErrorBoundary.js
+++ src/components/common/ErrorBoundary.js
@@ -1,7 +1,7 @@
 import React from "react";
 import * as Sentry from "@sentry/browser";
 
-class ErrorBoundary extends Component {
+class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
     this.state = { error: null };
